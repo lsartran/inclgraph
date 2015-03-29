a364e83f4a82508bb3e18dc110fe64a2b67b50a5
Laurent Sartran <lsartran@gmail.com>
2015-03-29 17:25:25 +0200

    Refactoring complete. Now we handle a list of include paths passed as -I dir on the command line.

--- inclgraph.hs
+++ inclgraph.hs
@@ -4,6 +4,7 @@ import Prelude hiding (FilePath)
 import Control.Applicative
 import Control.Monad
 import Data.Maybe
+import Data.Foldable (foldlM)
 import qualified Data.Map as Map
 import qualified Data.List as L
 import qualified Data.Text as T
@@ -14,9 +15,11 @@ import qualified Data.Attoparsec.Text as P
 import System.Environment (getArgs)
 import System.Directory (doesFileExist, getCurrentDirectory)
 import System.Console.GetOpt
+import System.IO.Error (catchIOError, isDoesNotExistError)
 import qualified Filesystem.Path.CurrentOS as Path
 import qualified Data.Graph.Inductive.Graph as G
 import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Graph.Inductive.Query.ArtPoint
 import qualified Data.GraphViz as GraphViz
 import qualified Data.GraphViz.Commands as GraphViz.Commands
 import qualified Data.GraphViz.Attributes as GraphViz.Attributes
@@ -40,6 +43,8 @@ data Include =
     | SourceFileInclude FilePath
     deriving (Show, Eq)
 
+filePathFromInclude (SystemHeaderInclude fp) = fp
+filePathFromInclude (SourceFileInclude fp) = fp
 
 notAIncludeLine :: P.Parser IncludeLine
 notAIncludeLine = liftM NotAIncludeLine $ P.skipWhile ((/=) '\n') <* (P.endOfLine <|> P.endOfInput)
@@ -66,92 +71,114 @@ includeLinesFromContents content =
         Left s -> error s
         Right l -> catMaybes $ map includeLines l
 
+--------------------------------------------------------------------------------
+-- INCLUSIONS PROCESSING
+--------------------------------------------------------------------------------
+
+-- Here, fp must point to a file that exists
 includeLinesFromFilePath :: FilePath -> IO [Include]
 includeLinesFromFilePath fp =
     fmap includeLinesFromContents $ TIO.readFile $ Path.encodeString $ fp
+{-
+        where
+            f name = catchIOError (g name) (\e -> if isDoesNotExistError e then return Nothing else ioError e) where
+            g name = do
+                cnt <- TIO.readFile name
+                return $ Just cnt
+-}
+
+findIncludeInDir :: FilePath -> Include -> IO (Maybe FilePath)
+findIncludeInDir includePath include =
+    let fp = Path.collapse $ Path.append includePath (filePathFromInclude include) in
+        do
+            exists <- doesFileExist (Path.encodeString fp)
+            case exists of
+                True -> return (Just fp)
+                False -> do
+                    return Nothing
+
+findInclude :: [FilePath] -> FilePath -> Include -> IO (Maybe FilePath)
+findInclude includePaths filePath include =
+    let actualIncludePaths = (Path.directory filePath):includePaths in
+        do
+            foundFiles <- mapM ((flip findIncludeInDir) include) actualIncludePaths
+            case catMaybes foundFiles of
+                [] -> do
+                    putStrLn $ "Warning: couldn't find " ++ (show include) ++ " included from " ++ (show filePath)
+                    return Nothing
+                x:xs -> return $ Just x
+
+includedFilesFromFilePath :: [FilePath] -> FilePath -> IO [FilePath]
+includedFilesFromFilePath includePaths filePath = do
+    includes <- includeLinesFromFilePath filePath
+    maybeIncludedFiles <- mapM (findInclude includePaths filePath) includes
+    return $ catMaybes maybeIncludedFiles
 
 --------------------------------------------------------------------------------
 -- GRAPH BUILDING
 --------------------------------------------------------------------------------
 
-newtype IncludeNode = IncludeNode (FilePath, [FilePath])
+newtype IncludeNode = IncludeNode (FilePath,[FilePath])
     deriving (Show, Eq, Ord)
 
 newtype IncludeGraph = IncludeGraph [IncludeNode]
     deriving (Show, Eq, Ord)
 
-emptyIncludeGraph = IncludeGraph []
+emptyGraph = IncludeGraph []
 
-{-
+isInGraph :: FilePath -> IncludeGraph -> Bool
+isInGraph fp (IncludeGraph nodes) =
+    isJust $ L.lookup fp [(fp, includedFiles) | IncludeNode (fp, includedFiles) <- nodes]
 
-addSourceFileToIncludeGraph :: IncludeGraph -> [Path.FilePath] -> Path.FilePath -> IO IncludeGraph
-addSourceFileToIncludeGraph graph searchpaths filepath =
-    let includelines = 
+addToGraph :: FilePath -> [FilePath] -> IncludeGraph -> IncludeGraph
+addToGraph fp included (IncludeGraph nodes) =
+    IncludeGraph $ (IncludeNode (fp,included)):nodes
 
-includeGraphFromFilename' :: IncludeGraph -> [Path.FilePath] -> Path.FilePath -> IO IncludeGraph
-includeGraphFromFilename' graph@(IncludeGraph adjlist) fp =
-    let encoded_fp = Path.encodeString fp in do
-        file_exists <- doesFileExist encoded_fp
-        if file_exists then do
-            --putStrLn encoded_fp
-            content <- TIO.readFile encoded_fp
-            nodes <- return $ includeNodesFromFileContents fp content
-            let new_graph = IncludeGraph $ (pathToSourceFileNode fp, nodes):adjlist in
-                includeGraphFromNodes' new_graph nodes
-        else
-            --putStrLn ("Warning: " ++ encoded_fp ++ " not found") >> return graph
-            putStrLn ("Warning: " ++ encoded_fp ++ " not found, adding anyway") >> return (IncludeGraph $ (pathToSourceFileNode fp, []):adjlist)
+buildGraphFromFile' :: [FilePath] -> FilePath -> IncludeGraph -> IO IncludeGraph
+buildGraphFromFile' includePaths sourceFile graph =
+    case (isInGraph sourceFile graph) of
+        True -> return graph -- nothing to do
+        False -> do
+            includedFiles <- includedFilesFromFilePath includePaths sourceFile
+            buildGraphFromFiles' includePaths includedFiles (addToGraph sourceFile includedFiles graph)
 
-includeGraphFromNodes' :: IncludeGraph -> [IncludeNode] -> IO IncludeGraph
-includeGraphFromNodes' graph [] = return graph
-includeGraphFromNodes' graph@(IncludeGraph adjlist) (n:ns) =
-    let action =
-            case (L.lookup n adjlist, n) of
-                (Just _, _) -> return graph
-                (Nothing, SourceFileNode fp) -> includeGraphFromFilename' graph fp
-                (Nothing, SystemHeaderNode _) -> return $ IncludeGraph adjlist -- $ IncludeGraph $ (n, []):graph_edges
-                --(Nothing, SystemHeaderNode _) -> return $ IncludeGraph $ (n, []):adjlist
-    in action >>= (flip includeGraphFromNodes' ns)
+buildGraphFromFiles' :: [FilePath] -> [FilePath] -> IncludeGraph -> IO IncludeGraph
+buildGraphFromFiles' includePaths sourceFiles graph = foldlM (flip (buildGraphFromFile' includePaths)) graph sourceFiles
 
-includeGraphFromNodes :: [IncludeNode] -> IO IncludeGraph
-includeGraphFromNodes = liftM normalisePaths . includeGraphFromNodes' (IncludeGraph [])
-
-normalisePathInNode :: Path.FilePath -> IncludeNode -> IncludeNode
-normalisePathInNode prefix node@(SystemHeaderNode _) = node
-normalisePathInNode prefix node@(SourceFileNode fp) =
+normalisePathInNode :: Path.FilePath -> Path.FilePath -> Path.FilePath
+normalisePathInNode prefix fp =
     case Path.stripPrefix prefix fp of
-        Nothing -> node ---trace (show (prefix, fp, node)) node
-        Just x -> SourceFileNode x
-
-filePathFromNode (SystemHeaderNode _) = Nothing
-filePathFromNode (SourceFileNode fp) = Just fp
+        Nothing -> fp ---trace (show (prefix, fp, node)) node
+        Just x -> x
 
 normalisePaths :: IncludeGraph -> IncludeGraph
-normalisePaths (IncludeGraph adjlist) =
-    let fps = Path.commonPrefix $ catMaybes $ map (filePathFromNode . fst) adjlist
+normalisePaths (IncludeGraph nodes) =
+    let fps = Path.commonPrefix [ fp | IncludeNode (fp,_) <- nodes ]
         norm = normalisePathInNode fps in
-    IncludeGraph [(norm a,[norm b | b <- l]) | (a,l) <- adjlist]
+    IncludeGraph [IncludeNode (norm a,[norm b | b <- l]) | IncludeNode (a,l) <- nodes]
+
+buildGraphFromFiles includePaths sourceFiles = normalisePaths <$> buildGraphFromFiles' includePaths sourceFiles emptyGraph
 
 --------------------------------------------------------------------------------
 -- GraphViz
 --------------------------------------------------------------------------------
 
-includeGraphToVerticesEdges :: IncludeGraph -> Gr IncludeNode () --([G.LNode IncludeNode], [G.UEdge])
+includeGraphToVerticesEdges :: IncludeGraph -> Gr Path.FilePath () --([G.LNode IncludeNode], [G.UEdge])
 includeGraphToVerticesEdges (IncludeGraph l) =
-    let lgraph_tmp = [(i,n,children) | (i,(n,children)) <- zip [1..] l]
+    let lgraph_tmp = [(i,n,children) | (i,IncludeNode (n,children)) <- zip [1..] l]
         lgraph_map = L.foldl' (\m (i,n,children) -> Map.insert n i m) Map.empty lgraph_tmp
         lgraph = [((i,n),[(fromJust j,c) | c <- children, let j = Map.lookup c lgraph_map, isJust j]) | (i,n,children) <- lgraph_tmp]
         lvertices = map fst lgraph
         ledges = [(i,j,()) | ((i,n),jl) <- lgraph, (j,_) <- jl] in
     G.mkGraph lvertices ledges
 
-formatNode (n, SystemHeaderNode t) = [GraphViz.Attributes.textLabel (TL.snoc (TL.cons '<' $ TL.fromStrict t) '>')]
+--formatNode (n, SystemHeaderNode t) = [GraphViz.Attributes.textLabel (TL.snoc (TL.cons '<' $ TL.fromStrict t) '>')]
 --formatNode (n, SourceFileNode fp) = [GraphViz.Attributes.textLabel (TL.snoc (TL.cons '"' $ TL.fromStrict $ Path.encode fp) '"')]
-formatNode (n, SourceFileNode fp) = [GraphViz.Attributes.textLabel $ TL.fromStrict $ Path.encode fp]
+formatNode (n, fp) = [GraphViz.Attributes.textLabel $ TL.fromStrict $ Path.encode fp]
 
-includeGraphToDot :: IncludeGraph -> GraphViz.DotGraph G.Node
+includeGraphToDot :: Gr Path.FilePath () -> GraphViz.DotGraph G.Node
 includeGraphToDot graph =
-    GraphViz.graphToDot params $ includeGraphToVerticesEdges graph
+    GraphViz.graphToDot params graph
     where
         params = GraphViz.nonClusteredParams { GraphViz.fmtNode = formatNode }
 
@@ -171,16 +198,20 @@ options =
 
 outp = OutputFile . fromMaybe "output.pdf"
 
-generateGraphVizFromFilenames :: [String] -> IO ()
-generateGraphVizFromFilenames fnames = do
-    inclgraph <- (includeGraphFromNodes . map (pathToSourceFileNode . Path.decodeString)) fnames
-    void $ GraphViz.Commands.runGraphvizCommand GraphViz.Commands.Dot (includeGraphToDot $ normalisePaths inclgraph) GraphViz.Commands.Pdf "output.pdf"
+generateGraphVizFromFilenames :: [String] -> [String] -> IO ()
+generateGraphVizFromFilenames includeDirs fnames = do
+    inclgraph <- buildGraphFromFiles (map Path.decodeString includeDirs) (map Path.decodeString fnames)
+    void $ GraphViz.Commands.runGraphvizCommand GraphViz.Commands.Dot (includeGraphToDot $ includeGraphToVerticesEdges inclgraph) GraphViz.Commands.Pdf "output.pdf"
+
+getIncludes [] = []
+getIncludes ((IncludeDir dir):opts) = dir:(getIncludes opts)
+getIncludes ((_):opts) = (getIncludes opts)
 
 main = do
     args <- getArgs
     case getOpt Permute options args of
-        (o,n,[]) -> generateGraphVizFromFilenames n
+        (o,n,[]) -> do
+            --putStrLn $ show o
+            generateGraphVizFromFilenames (getIncludes o) n
         (_,_,errs) -> ioError (userError $ concat errs ++ usageInfo header options)
     where header = "Usage: inclgraph [OPTIONS...] files"
-
--}
